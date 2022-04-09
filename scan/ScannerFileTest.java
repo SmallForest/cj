9324b7c99b9d148d3f78c1c5df240d74624f5494
smallforest <1032817724@qq.com>
2022-04-09 21:36:34 +0800

    del txt

--- scan/ScannerFileTest.java
+++ scan/ScannerFileTest.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class ScannerFileTest {
     public static void main(String[] args) throws Exception {
-        var sc = new Scanner(new File("ScannerFileTest.java"));
+        var sc = new Scanner(new File("scan/ScannerFileTest.java"));
         while(sc.hasNextLine()){
             System.out.println(sc.nextLine());
         }
--- sys/SystemTest.java
+++ sys/SystemTest.java
@@ -1,13 +1,23 @@
 package sys;
 
+import java.io.FileOutputStream;
 import java.util.Map;
+import java.util.Properties;
 
 public class SystemTest{
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        //获取系统所有的环境变量
         Map<String,String> env = System.getenv();
         for (String name : env.keySet()) {
             System.out.println(name+'='+env.get(name));
         }
+        //获取指定环境变量的值
+        System.out.println(System.getenv("JAVA_HOME"));
+        //获取搜索系统属性
+        Properties props = System.getProperties();
+        //将所有系统属性保存到 props.txt 中
+        props.store(new FileOutputStream("props.txt"),"System Properties");
+        //输出特定的系统属性
+        System.out.println(System.getProperty("os.name"));
     }
 }
